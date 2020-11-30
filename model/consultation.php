25e7206fff8bff5e49297acd9491e9dfe7f83b11
Doniasarsar <donia.sarsar@esprit.tn>
2020-11-30 22:30:44 +0100

    model consultation.php

--- model/consultation.php
+++ model/consultation.php
@@ -1,11 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+ 	class Consultation{
+ 		
+ 		 private int $num_rdv;
+		 private date $date_rdv;
+		 private string $objet_rdv;
+
+ 		function __consrtruct(date $date_rdv, string $objet_rdv){
+
+ 			$this->date_rdv = $date_rdv;
+ 			$this->objet_rdv = $objet_rdv;
+ 		}
+
+ 		function getID(): int{
+ 			return $this->num_rdv;
+ 		}
+
+ 		function getDate_rdv(): date{
+ 			return $this->date_rdv; 
+ 		}
+
+ 		function getObjet_rdv(): string{
+ 			return $this->objet_rdv; 
+ 		}
+
+ 		function setID(int $num_rdv): void{
+ 			$this->num_rdv;
+ 		}
+
+ 		function setDate_rdv(string $date_rdv): void{
+ 			$this->date_rdv;
+ 		}
+
+ 		function setObjet_rdv(string $objet_rdv): void{
+ 			$this->objet_rdv;
+ 		}
+ 	} 
+ ?>
