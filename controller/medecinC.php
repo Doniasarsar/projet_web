66e0c7aca50f4c8a658b87913cc4a96f499f7a22
yass24 <74965471+yass24@users.noreply.github.com>
2021-01-06 19:22:27 +0100

    Add files via upload

--- controller/medecinC.php
+++ controller/medecinC.php
@@ -4,19 +4,7 @@
 
 	class medecinC {
 		
-		function affichermedecins($Login_medecin){
-			
-			$sql="SELECT * FROM medecin where Login_medecin='$Login_medecin'";
-			$db = config::getConnexion();
-			try{
-				$liste = $db->query($sql);
-				return $liste;
-			}
-			catch (Exception $e){
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
-			}	
-		}
-		function affichermedecin(){
+		function affichermedecins(){
 			
 			$sql="SELECT * FROM medecin";
 			$db = config::getConnexion();
@@ -25,115 +13,76 @@
 				return $liste;
 			}
 			catch (Exception $e){
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
+				die('Erreur: '.$e->getMessage());
 			}	
 		}
 
 		function ajoutermedecin($medecin){
-			$sql="INSERT INTO medecin (CIN_medecin,Nom_medecin,Prenom_medecin,date_medecin,specialite,fonction,Login_medecin,MDP_medecin,phone_medecin,Adresse_medecin) 
-			      VALUES (:v1, :v2, :v3, :v4, :v5, :v6, :v7, :v8, :v9, :v10)";
+			$sql="INSERT INTO medecin (idmedecin, nom, prenom, numero_de_telephone,Login_medecin) 
+			      VALUES (:v1, :v2, :v3, :v4, :v5)";
 			$db = config::getConnexion();
 			try{
 				$query = $db->prepare($sql);
+			
 				$query->execute([
-					'v1' => $medecin->getCIN_medecin(),
-					'v2' => $medecin->getNom_medecin(),
-					'v3' => $medecin->getPrenom_medecin(),
-					'v4' => $medecin->getdate_medecin(),
-					'v5' => $medecin->getspecialite(),
-					'v6' => $medecin->getnbreMedecin(),
-					'v7' => $medecin->getLogin_medecin(),
-					'v8' => $medecin->getMDP_medecin(),
-					'v9' => $medecin->getphone_medecin(),
-					'v10' => $medecin->getAdresse_medecin()
-				]);	
+					'v1' => $medecin->getIdmedecin(),
+					'v2' => $medecin->getnom(),
+					'v3' => $medecin->getprenom(),
+					'v4' => $medecin->getnumero_de_telephone(),
+					'v5' => $medecin->getLogin_medecin()
+				]);
 				$destinataire = $medecin->getLogin_medecin();
 				$sujet = "Activer votre compte" ;
-				$entete = "From: TogetherVersusCovid@gmail.com" ;
- 				$message = 'Bienvenue sur votre plateform:TogetherVersusCovid,
-			
-				Mr/Mme '.$medecin->getNom_medecin().$medecin->getPrenom_medecin().
-				
-				
-				'              
-				votre login est :'.$medecin->getLogin_medecin().
-				'              				
-				votre mots de passe est :'.$medecin->getMDP_medecin().
-				'
-				Pour acceder à votre compte, veuillez cliquer sur le lien ci-dessous
-				ou copier/coller dans votre navigateur Internet.
-				
-				http://localhost/new%20projet%20web/views/front/login_medecin.php';
-				
-				
+				$entete = "From: mestiriyassine69@gmail.com" ;
+ 				$message = 'Bienvenue sur TogetherVersusCovid,'.$medecin->getLogin_medecin();
+				 ini_set('SMTP','smtp.topnet.tn');
  				mail($destinataire, $sujet, $message, $entete);
-				echo "<script language=\"javascript\"> alert(\"Nouveau medecin a été créé avec succès\");</script>";
-				echo "<html><head><title>Redirection en HTML</title>
-                          <meta http-equiv=\"refresh\" content=\"0; URL=.\\showmedecin.php\">
-				</head><body></body></html>";
-			
+						
+                echo " Nouveau medecin a été créé avec succès <br>";				
 			}
-			catch (Exception $e){
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
-			}		
 			
+			catch (Exception $e){
+				echo 'Erreur: '.$e->getMessage();
+			}			
 		}
 		
-		function supprimermedecin($CIN_medecin){
-			$sql="DELETE FROM medecin WHERE CIN_medecin= :v1";
+		function supprimermedecin($idmedecin){
+			$sql="DELETE FROM medecin WHERE idmedecin= :v1";
 			$db = config::getConnexion();
 			$req=$db->prepare($sql);
-			$req->bindValue(':v1',$CIN_medecin);
-			echo "<script language=\"javascript\"> alert(\"medecin supprimer avec succès\");</script>";
-			
-					echo "<html><head><title>Redirection en HTML</title>
-                          <meta http-equiv=\"refresh\" content=\"0; URL=.\\front\\index.php\">
-                          </head><body></body></html>";
+			$req->bindValue(':v1',$idmedecin);
 			try{
 				$req->execute();
 			}
 			catch (Exception $e){
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
+				die('Erreur: '.$e->getMessage());
 			}
 		}
 		
-		function modifiermedecin($medecin){
+		function modifiermedecin($medecin, $idmedecin){
 			try {
 				$db = config::getConnexion();
 				$query = $db->prepare(
 					'UPDATE medecin SET 
-						Nom_medecin = :v1, 
-						Prenom_medecin = :v2,
-						date_medecin = :v3,
-						Login_medecin = :v4,
-						MDP_medecin = :v5,
-						phone_medecin = :v6,
-						Adresse_medecin = :v7
-					WHERE CIN_medecin = :v8'
+						nom = :v1, 
+						prenom = :v2,
+						numero_de_telephone = :v3
+					WHERE idmedecin = :v4'
 				);
 				$query->execute([
-					
-					'v1' => $medecin->getNom_medecin(),
-					'v2' => $medecin->getPrenom_medecin(),
-					'v3' => $medecin->getdate_medecin(),
-					'v4' => $medecin->getLogin_medecin(),
-					'v5' => $medecin->getMDP_medecin(),
-					'v6' => $medecin->getphone_medecin(),
-					'v7' => $medecin->getAdresse_medecin(),
-					'v8' => $medecin->getCIN_medecin()	
+					'v1' => $medecin->getnom(),
+					'v2' => $medecin->getprenom(),
+					'v3' => $medecin->getnumero_de_telephone(),
+					'v4' => $medecin->getIdmedecin(),
+					'v5' => $medecin->getLogin_medecin()					
 				]);
-				$login = $medecin->getLogin_medecin();
-				echo "<script language=\"javascript\"> alert(\"medecin modifier avec succès\");</script>";
-			
-					echo  "<html><head><title>Redirection en HTML</title>
-                          <meta http-equiv=\"refresh\" content=\"0; URL=.\\showmedecins.php?login=$login\">
-                          </head><body></body></html>";
+				echo $query->rowCount() . " medecins modifiés avec succès <br>";
 			} catch (PDOException $e) {
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
+				$e->getMessage();
 			}
 		}
-		function recuperermedecin($CIN_medecin){
-			$sql="SELECT * from medecin where CIN_medecin=$CIN_medecin";
+		function recuperermedecin($idmedecin){
+			$sql="SELECT * from medecin where idmedecin=$idmedecin";
 			$db = config::getConnexion();
 			try{
 				$query=$db->prepare($sql);
@@ -143,11 +92,11 @@
 				return $medecin;
 			}
 			catch (Exception $e){
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
+				die('Erreur: '.$e->getMessage());
 			}
 		}
-		function recherchermedecin($CIN_medecin){
-			$sql="SELECT count(*) AS nbre from medecin where CIN_medecin=$CIN_medecin";
+		function recherchermedecin($idmedecin){
+			$sql="SELECT count(*) AS nbre from medecin where idmedecin=$idmedecin";
 			$db = config::getConnexion();
 			try{
 				$query=$db->prepare($sql);
@@ -155,44 +104,13 @@
 
 				$medecin=$query->fetch();
 				if ($medecin['nbre'] == 0)
-				  echo "<script language=\"javascript\"> alert(\"medecin $CIN_medecin inexistant\");</script>";
+				  echo "<script language=\"javascript\"> alert(\"medecin $idmedecin inexistant\");</script>";
 				else 
-				  echo "<script language=\"javascript\"> alert(\"medecin $CIN_medecin existe dans la BD\");</script>";
+				  echo "<script language=\"javascript\"> alert(\"medecin $idmedecin existe dans la BD\");</script>";
 			}
 			catch (Exception $e){
-				echo '<h3>Erreur: '.$e->getMessage().'</h3>';
+				die('Erreur: '.$e->getMessage());
 			}
 		}
-		function verification_MDP($Login_medecin,$MDP_medecin){
-				$sql="SELECT count(*) AS nbre from medecin where Login_medecin='$Login_medecin' and MDP_medecin='$MDP_medecin'";
-				$db = config::getConnexion();
-				try{
-					$query=$db->prepare($sql);
-					$query->execute();
-
-					$medecin=$query->fetch();
-					if ($medecin['nbre'] == 0){
-					echo "<script language=\"javascript\"> alert(\"Incorrect Login or Password\");</script>";
-			
-					echo "<html><head><title>Redirection en HTML</title>
-                          <meta http-equiv=\"refresh\" content=\"0; URL=.\\front\\Login_medecin.php\">
-                          </head><body></body></html>";
-					}
-					else 
-					
-					echo  "<html><head><title>Redirection en HTML</title>
-                          <meta http-equiv=\"refresh\" content=\"0; URL=.\\showmedecins.php?login=$Login_medecin\">
-                          </head><body></body></html>";
-						  
-					}
-					catch (Exception $e){
-						echo '<h3>Erreur: '.$e->getMessage().'</h3>';
-										}
-										
-				
-			}
-		
-}
-		
-		
+	}
 ?>
